fccc4ba4d57c27139d88da3a391429d16e12f0e5
Ecort-hue <joshuacortes017@gmail.com>
2026-03-26 12:21:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var questionText = document.getElementById("question");
+var fetchedQuestion;
+
+
+
+async function getQuestion() {
+    fetch ("https://api.truthordarebot.xyz/api/wyr")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        fetchedQuestion = data['question'];
+        console.log(fetchedQuestion);
+        questionText.innerHTML = JSON.stringify(fetchedQuestion);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
+getQuestion()
