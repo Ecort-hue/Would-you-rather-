d248f36a52fce3a9fedb3214fa8f668bf1223b81
Ecort-hue <joshuacortes017@gmail.com>
2026-03-31 12:20:01 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-var questionText = document.getElementById("question");
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+
 var fetchedQuestion;
 
-
+function getOptions() {
+    let question = fetchedQuestion;
+    question = question.replace(/^Would you rather\s*/i, "");
+    let options = question.split(/\s+or\s+/i);
+    return {
+        option1: options[0]?.trim(),
+        option2: options[1]?.trim().replace(/\?$/, "")
+    };
+}
 
 async function getQuestion() {
-    fetch ("https://api.truthordarebot.xyz/api/wyr")
+    fetch("https://api.truthordarebot.xyz/api/wyr")
     .then(function (response) {
+        console.log(response);
         return response.json();
-        
     })
     .then(function (data) {
         fetchedQuestion = data['question'];
-        console.log(fetchedQuestion);
-        questionText.innerHTML = JSON.stringify(fetchedQuestion);
+
+        const options = getOptions();
+        option1.innerHTML = options.option1;
+        option2.innerHTML = options.option2;
     })
     .catch(function (error) {
         console.log(error);
     });
 }
-getQuestion()
+
+getQuestion();
+
+const option1Button = document.getElementById("option1");
+
+option1Button.addEventListener('click', () => {
+    window.location.href = 'option1.html';
+});
---
+++ start.js
@@ -0,0 +1,5 @@
+const beginButton = document.getElementById("beginButton");
+
+beginButton.addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
